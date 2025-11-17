58fe9c2f9bdfd5fb08b56b929494542ddbfce3ae
briiel <144537019+briiel@users.noreply.github.com>
2025-11-17 16:26:05 +0800

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -10,32 +10,9 @@ const db = mysql.createPool({
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    // Connection pool resilience settings to prevent ECONNRESET errors
-    enableKeepAlive: true,
-    keepAliveInitialDelay: 0,
-    // Timeout for connection establishment (in milliseconds)
-    connectTimeout: 60000,
-    // Maximum number of milliseconds to wait when checking out a connection
-    acquireTimeout: 60000,
-    // Maximum lifetime of a connection in the pool (10 minutes)
-    maxIdle: 600000,
-    // How often to check for idle connections to kill (in milliseconds)
-    idleTimeout: 60000,
-    // ssl: {
-    //     rejectUnauthorized: false
-    // }
-});
-
-// Handle connection errors gracefully
-db.on('connection', (connection) => {
-    console.log('New DB connection established');
-    
-    connection.on('error', (err) => {
-        console.error('Database connection error:', err);
-        if (err.code === 'PROTOCOL_CONNECTION_LOST' || err.code === 'ECONNRESET') {
-            console.log('Connection lost, will be recycled');
-        }
-    });
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = db.promise();
